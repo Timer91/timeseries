3915e9c5651de98c64cad404dd65814fae65cb1d
Timer91 <marais.jeremie@gmail.com>
2018-06-14 05:31:51 +0200

    Coding style (80 columns, ...) + ES6

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+"use strict";
+
 function createSeason( timeline ) {
 	const pxPerMonth = timeline.pxPerMonth(),
 		pxPerWeek = pxPerMonth * 12 / 52;
 
 	timeline.rootElement.innerHTML = "";
-	Object.entries( series ).forEach( function( serie ) {
+	Object.entries( series ).forEach( serie => {
 		const elTitle = document.createElement( "div" ),
 			elSerie = document.createElement( "div" );
 
 		elTitle.classList.add( "title" );
 		elTitle.innerHTML = serie[ 0 ];
 
-		serie[ 1 ].seasons.forEach( function( s ) {
+		serie[ 1 ].seasons.forEach( s => {
 			const elSeason = document.createElement( "div" ),
 				sMonth = timeline.monthsBetween( minYear, s.firstAired );
 
@@ -26,12 +28,13 @@ function createSeason( timeline ) {
 	});
 }
 
-window.onresize = function() {
+window.onresize = _ => {
 	timeline.resize.call( timeline );
 	createSeason( timeline );
+	return false;
 }
 
-const minYear = 2005,
+const minYear = 2004,
 	maxYear = 2020,
 	elTimeline = document.getElementById( "timelines" ),
 	timeline = new Timeline( elTimeline, minYear, maxYear,
--- timeline.js
+++ timeline.js
@@ -3,11 +3,11 @@
 class Timeline {
 	constructor( el, start, end, cur ) {
 		this.rootElement = el;
-		this._years = { 
+		this._years = {
 			start, end, cur,
 			inMonth: ( end - start ) * 12 };
 		this._color = {
-			cur: "rgba( 221, 204, 51, .8 )",
+			cur: "rgba( 255, 255, 255, .1 )",
 			year: "rgba( 0, 0, 0, .5 )",
 			month: "rgba( 0, 0, 0, .2 )" }
 		this._viewBox = this.rootElement.offsetWidth;
@@ -22,7 +22,7 @@ class Timeline {
 	}
 	resize() {
 		this._viewBox = this.rootElement.offsetWidth;
-		this.rootElement.style.backgroundSize = `${ this._viewBox }px 1px`;
+		this.rootElement.style.backgroundSize = `${this._viewBox}px 1px`;
 		this.render();
 	}
 	render() {
@@ -33,18 +33,25 @@ class Timeline {
 	_render() {
 		const el = this.rootElement,
 			monthPx = this.pxPerMonth(),
-			curMonth = this.monthsBetween( this._years.start, this._years.cur ),
-			steps = [ `<rect x='0' height='1px' width='1px' fill='${ this._color.year }'/>`,
-			, `<rect height='1px' width='1px' x='${ monthPx * curMonth }' fill='${ this._color.cur }'/>` ];
+			curMonth = this.monthsBetween(
+				this._years.start,
+				this._years.cur ),
+			steps = [
+				`<rect x='0' height='1px' width='1px'
+					fill='${ this._color.year }'/>`,
+				`<rect height='1px' width='3px' x='${ monthPx * curMonth }'
+					fill='${ this._color.cur }'/>` ];
 
 		for ( let month = 1; month < this._years.inMonth; ++month ) {
-			steps.push( `<rect height='1px' width='1px' x='${ monthPx * month }' 
-				fill='${ month % 12 ? this._color.month : this._color.year }' />` );
+			steps.push( `<rect height='1px' width='1px'
+				x='${monthPx * month}' fill='${
+				month % 12 ? this._color.month : this._color.year}' />` );
 		}
 		el.style.backgroundImage = `url("${ encodeURI(
-			"data:image/svg+xml,<svg preserveAspectRatio='none' xmlns='http://www.w3.org/2000/svg'" +
-			` viewBox='0 0 ${ this._viewBox } 1'>${ steps.join( " " ) }</svg>`
-		) }")`;
-		el.style.backgroundSize = `${ this._viewBox }px 1px`;
+			"data:image/svg+xml,<svg preserveAspectRatio='none' " +
+			"xmlns='http://www.w3.org/2000/svg'" +
+			` viewBox='0 0 ${this._viewBox} 1'>` +
+			`${steps.join( " " ) }</svg>` )}")`;
+		el.style.backgroundSize = `${this._viewBox}px 1px`;
 	}
 }
